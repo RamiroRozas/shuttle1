88d1a9a553a5b2726e4f6cacee6f0ba9eabd90f7
RamiroRozas <ramiro.rozas87@gmail.com>
2017-01-18 19:00:37 -0500

    vista2

--- resources/views/administrador/master.blade.php
+++ resources/views/administrador/master.blade.php
@@ -3,7 +3,18 @@
  <link rel="stylesheet" href="{{URL::asset('assets/css/fullcalendar.min.css')}}">
  <link rel="stylesheet" href="{{URL::asset('assets/css/fullcalendar.print.css')}}" media="print">
  @stop
- @section('scripts')
- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+ @section('scripts')  
+ <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
  <script src="{{URL::asset('assets/js/fullcalendar.min.js')}}"></script> 
+ <script>
+ 	$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+});
+ </script>
  @stop
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -21,10 +21,6 @@
    @yield('content')  
 <!-- Scripts -->
     @yield('scripts')
-<script
-  src="https://code.jquery.com/jquery-3.1.1.min.js"
-  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-  crossorigin="anonymous"></script>
 <script src="{{URL::asset('assets/js/bootstrap.js')}}"></script>
 </body>
 </html>
